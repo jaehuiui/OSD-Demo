23932b83ac7b9ff8d51c752a798f84fb953e83ba
jaehuiui <ljh9081@kaist.ac.kr>
2021-12-16 18:07:59 +0900

    Chore : Configuration with typescript, linting

--- webpack.common.js
+++ webpack.common.js
@@ -1,57 +1,72 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
 
 module.exports = {
-  entry: "./src/pages/index.js",
+	entry: "./src/pages/index.js",
 
-  resolve: {
-    fallback: {
-      fs: false,
-    },
-  },
+	resolve: {
+		fallback: {
+			fs: false,
+		},
+	},
 
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
-      },
-      {
-        test: /\.(png|jp(e*)g|svg|gif)$/,
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-              name: "images/[hash]-[name].[ext]",
-            },
-          },
-        ],
-      },
-      {
-        test: /\.(s[ac]ss|css)$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.(ttf|woff|eot)$/,
-        use: {
-          loader: "file-loader",
-        },
-      },
-    ],
-  },
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+				},
+			},
+			{
+				test: /\.(ts|tsx)$/,
+				use: [
+					"babel-loader",
+					{
+						loader: "ts-loader",
+						options: {
+							transpileOnly: true,
+						},
+					},
+				],
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.(png|jp(e*)g|svg|gif)$/,
+				use: [
+					{
+						loader: "file-loader",
+						options: {
+							name: "images/[hash]-[name].[ext]",
+						},
+					},
+				],
+			},
+			{
+				test: /\.(s[ac]ss|css)$/,
+				use: ["style-loader", "css-loader", "sass-loader"],
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.(ttf|woff|eot)$/,
+				use: {
+					loader: "file-loader",
+				},
+			},
+		],
+	},
 
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "public/index.html"),
-      filename: "index.html",
-    }),
-    new MiniCssExtractPlugin({
-      filename: "[name].[hash:16].css",
-    }),
-  ],
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.resolve(__dirname, "public/index.html"),
+			filename: "index.html",
+		}),
+		new MiniCssExtractPlugin({
+			filename: "[name].[hash:16].css",
+		}),
+		new ForkTsCheckerWebpackPlugin(),
+	],
 };
